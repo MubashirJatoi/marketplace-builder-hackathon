aedc1516df1b65e5538435ec3fd933ea4ae3db61
Rimsha Arshad <adeelarshad8900@gmail.com>
2025-02-17 19:50:01 +0500

    hello

--- e-commerce/src/components/header.tsx
+++ e-commerce/src/components/header.tsx
@@ -6,18 +6,18 @@ function Header() {
 
 
     return(
-        <div className="w-[100%] h-[36px] items-center bg-[#F5F5F5] flex justify-between pl-[48px] pr-[48px]">
+        <div className="w-[100%] h-[36px] items-center bg-[#F5F5F5] flex justify-between pl-[48px] pr-[48px] mx-auto">
             <div className="cursor-pointer">
                 <Link href={"/"}>
                     <Image src={Logo} alt="logo"></Image>
                 </Link>
             </div>
-            <div>
+            <div className="hidden sm:hidden md:block">
             <Link href={"/shop"}>
                 <p className="text-[13px] cursor-pointer flex items-center pb-[3px] justify-center hover:rounded-[8px] w-[146.86px] h-[26px] hover:bg-black text-center hover:text-[#FFFFFF] bg-[#FFFFFF] ml-[400px] font-[500] text-[#111111]">Skip to main content</p>
             </Link>
             </div>
-            <div className="flex gap-5">
+            <div className="flex gap-3 md:gap-5">
                 <Link href={"/location"}>
                     <p className="text-[11px] font-[500] text-[#111111] cursor-pointer hover:underline">Find a Store</p>
                 </Link>
--- e-commerce/src/components/navbar.tsx
+++ e-commerce/src/components/navbar.tsx
@@ -1,40 +1,104 @@
+"use client";
+import { useState } from "react";
 import Image from "next/image";
-import Nike from "../../public/Images/nike-logo.png"
-import Like from "../../public/Images/like.png"
-import Buy from "../../public/Images/buy.png"
-import Search from "../../public/Images/search.png"
+import Nike from "../../public/Images/nike-logo.png";
+import Like from "../../public/Images/like.png";
+import Buy from "../../public/Images/buy.png";
+import Search from "../../public/Images/search.png";
 import Link from "next/link";
 
 function Navbar() {
-    return(
-        <div className="flex w-[100%] h-[60px] items-center">
-            <div className="flex items-center justify-around h-[60px] w-[95%] m-auto">
-            <div>
-            <Link href={"/"}>
-                <Image className="cursor-pointer" src={Nike} alt="nike"></Image>
-            </Link>
-            </div>
-            <div className="flex text-[15px] ml-[130px] font-[600] gap-6">
-                <p className="cursor-pointer hover:underline">New & Featured</p>
-                <p className="cursor-pointer hover:underline">Men</p>
-                <p className="cursor-pointer hover:underline">Women</p>
-                <p className="cursor-pointer hover:underline">Kids</p>
-                <p className="cursor-pointer hover:underline">Sale</p>
-                <p className="cursor-pointer hover:underline">SNKRS</p>
-            </div>
-            <div className="mt-[-10px] mr-[-120px]">
-                <Image className="cursor-pointer relative top-[29px] left-[15px]" src={Search} alt="search"></Image>
-                <input className="w-[180px] pl-[50px] bg-[#F5F5F5] h-[40px] rounded-[100px]" type="text" placeholder="Search"/>
-            </div>
-            <div className="flex gap-5">
-            <Image className="cursor-pointer hover:rounded-[4px] hover:bg-[#dddbdb]" src={Like} alt="like"></Image>
-            <Link href={"/cart"}>
-                <Image className="cursor-pointer hover:rounded-[4px] hover:bg-[#dddbdb]" src={Buy} alt="buy"></Image>
-            </Link>
-            </div>
-            </div>
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <div className="flex w-[100%] h-[60px] items-center">
+      <div className="flex items-center justify-around h-[60px] w-[95%] m-auto">
+        <div
+          className="flex items-center gap-6 md:hidden cursor-pointer"
+          onClick={toggleMenu}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M4 6h16M4 12h16m-7 6h7"
+            />
+          </svg>
+          <Link href={"/"}>
+            <Image className="cursor-pointer" src={Nike} alt="nike"></Image>
+          </Link>
         </div>
-    );
-};
+        <div>
+          <Link href={"/"}>
+            <Image
+              className="cursor-pointer hidden md:block"
+              src={Nike}
+              alt="nike"
+            ></Image>
+          </Link>
+        </div>
+        <div className="hidden md:flex text-[15px] ml-[130px] font-[600] gap-6">
+          <Link href={"#"} className="cursor-pointer hover:underline">New & Featured</Link>
+          <Link href={"#"} className="cursor-pointer hover:underline">Men</Link>
+          <Link href={"#"} className="cursor-pointer hover:underline">Women</Link>
+          <Link href={"#"} className="cursor-pointer hover:underline">Kids</Link>
+          <Link href={"#"} className="cursor-pointer hover:underline">Sale</Link>
+          <Link href={"#"} className="cursor-pointer hover:underline">SNKRS</Link>
+        </div>
+        {/* Menu Items - Hidden on Mobile */}
+        <div className={`md:hidden text-[15px] mt-[60px] bg-black text-white p-10 font-[600] gap-10 w-[100%] absolute top-10 left-0 z-10 ${menuOpen ? 'block' : 'hidden'}`}>
+                    <Link href={"#"} className="cursor-pointer hover:underline">New & Featured</Link>
+                    <Link href={"#"} className="cursor-pointer hover:underline">Men</Link>
+                    <Link href={"#"} className="cursor-pointer hover:underline">Women</Link>
+                    <Link href={"#"} className="cursor-pointer hover:underline">Kids</Link>
+                    <Link href={"#"} className="cursor-pointer hover:underline">Sale</Link>
+                    <Link href={"#"} className="cursor-pointer hover:underline">SNKRS</Link>
+                </div>
+        <div className="hidden md:block mt-[-10px] mr-[-120px]">
+          <Image
+            className="cursor-pointer relative top-[29px] left-[15px]"
+            src={Search}
+            alt="search"
+          ></Image>
+          <input
+            className="w-[180px] pl-[50px] bg-[#F5F5F5] h-[40px] rounded-[100px]"
+            type="text"
+            placeholder="Search"
+          />
+        </div>
+        <div className="flex gap-5">
+          <Image
+            className="md:hidden cursor-pointer relative"
+            src={Search}
+            alt="search"
+          ></Image>
+          <Image
+            className="cursor-pointer hover:rounded-[4px] hover:bg-[#dddbdb]"
+            src={Like}
+            alt="like"
+          ></Image>
+          <Link href={"/cart"}>
+            <Image
+              className="cursor-pointer hover:rounded-[4px] hover:bg-[#dddbdb]"
+              src={Buy}
+              alt="buy"
+            ></Image>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default Navbar;
