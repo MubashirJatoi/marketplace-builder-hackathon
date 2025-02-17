444d419cbb18db75d0d017b12b9adcc7c8dce51b
Rimsha Arshad <adeelarshad8900@gmail.com>
2025-02-17 20:37:34 +0500

    hello

--- e-commerce/src/components/footer.tsx
+++ e-commerce/src/components/footer.tsx
@@ -8,9 +8,9 @@ import Link from "next/link";
 
 function Footer() {
     return(
-        <div className="bg-[#111111] h-[331px]">
-            <div>
-                <div className="flex pt-[30px] pl-[30px]">
+        <div className="bg-[#111111] h-auto pb-4">
+            <div className="w-[90%] m-auto">
+                <div className="grid grid-cols-2 gap-6 md:grid-cols-4 justify-center pt-[30px] pl-[30px]">
                     <div className="text-[11px] text-[#FFFFFF] font-[500] leading-[32.67px]">
                         <p className="cursor-pointer hover:text-[#7E7E7E]">Find A Store</p>
                         <Link href={"/joinUs"}>
@@ -22,7 +22,7 @@ function Footer() {
                         <p className="cursor-pointer hover:text-[#7E7E7E]">Send Us Feedback</p>
                         <p className="cursor-pointer hover:text-[#7E7E7E]">Student Discounts</p>
                     </div>
-                    <div className="text-[11px] ml-[200px] font-[400] text-[#7E7E7E] leading-[28px]">
+                    <div className="text-[11px] font-[400] text-[#7E7E7E] leading-[28px]">
                         <p className="text-[#FFFFFF] text-[11px] font-[500]">Get Help</p>
                         <Link href={"/order"}>
                             <p className="cursor-pointer hover:text-[#FFFFFF]">Order Status</p>
@@ -37,7 +37,7 @@ function Footer() {
                         <p className="cursor-pointer hover:text-[#FFFFFF]">Contact Us On Nike.com Inquiries</p>
                         <p className="cursor-pointer hover:text-[#FFFFFF]">Contact Us On All Other Inquiries</p>
                     </div>
-                    <div className="text-[#7E7E7E] ml-[100px] text-[12px] leading-[28px]">
+                    <div className="text-[#7E7E7E] text-[12px] leading-[28px]">
                         <p className="text-[#FFFFFF] text-[11px] font-[500]">About Nike</p>
                         <p className="cursor-pointer hover:text-[#FFFFFF]">News</p>
                         <p className="cursor-pointer hover:text-[#FFFFFF]">Careers</p>
@@ -45,23 +45,26 @@ function Footer() {
                         <p className="cursor-pointer hover:text-[#FFFFFF]">Sustainability</p>
                     </div>
                 </div>
-                <div className="flex gap-3 justify-end relative bottom-[190px] right-[30px]">
+                <div className="flex gap-3 justify-end relative bottom-[120px] md:bottom-[190px] md:right-[30px]">
                     <Image className="cursor-pointer transition-transform duration-[0.3s] ease-in-out hover:transform hover:scale-105" src={Twitter} alt="twitter"></Image>
                     <Image className="cursor-pointer transition-transform duration-[0.3s] ease-in-out hover:transform hover:scale-105" src={Facebook} alt="facebook"></Image>
                     <Image className="cursor-pointer transition-transform duration-[0.3s] ease-in-out hover:transform hover:scale-105" src={Youtube} alt="youtube"></Image>
                     <Image className="cursor-pointer transition-transform duration-[0.3s] ease-in-out hover:transform hover:scale-105" src={Instagram} alt="instagram"></Image>
                 </div>
             </div>
-            <div>
-                <div className="flex gap-3 pl-[30px] mt-[40px]">
+            <div className="flex flex-col md:flex-row justify-between w-[95%] m-auto">
+                <div className="flex gap-10 justify-center">
+                    <div className="flex gap-2">
                     <Link href={"/location"}>
                         <Image className="w-[9.38px] cursor-pointer transition-transform duration-[0.3s] ease-in-out hover:transform hover:scale-105 h-[13.12px] mt-[3px]" src={Location} alt="location"></Image>
                     </Link>
                     <Link href={"/location"}>
-                        <p className="text-[#FFFFFF] ml-[-5px] hover:text-[#7E7E7E] cursor-pointer text-[12px]">India</p>
+                        <p className="text-[#FFFFFF] hover:text-[#7E7E7E] cursor-pointer text-[12px]">India</p>
                     </Link>
-                <p className="text-[#7E7E7E] ml-[20px] text-[11px] ">© 2023 Nike, Inc. All Rights Reserved</p>
-                <div className="flex ml-[650px] gap-5 text-[12px] text-[#7E7E7E] text-center">
+                    </div>
+                    <p className="text-[#7E7E7E] text-[11px] ">© 2023 Nike, Inc. All Rights Reserved</p>
+                </div>
+                <div className="flex gap-2 md:gap-5 justify-center text-[12px] text-[#7E7E7E] text-center">
                     <Link  href={"/help"}>
                         <p className="cursor-pointer hover:text-[#FFFFFF]">Guides</p>
                     </Link>
@@ -74,7 +77,6 @@ function Footer() {
                     <Link  href={"/help"}>
                         <p className="cursor-pointer hover:text-[#FFFFFF]">Nike Privacy Policy</p>
                     </Link>
-                </div>
                 </div>
             </div>
         </div>
